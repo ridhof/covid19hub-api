6a676acbd07da3c879635f0b6fe5578977c1985b
Ridho Febriansa <officialridhof@gmail.com>
2020-03-24 17:14:03 +0700

    display necessary attributes, add _baru on some attributes

--- api/nasional/today.js
+++ api/nasional/today.js
@@ -19,9 +19,24 @@ module.exports = (req, res) => {
         } else {
             const db = client.db('covid19hubid');
             
-            db.collection('timeline_summary').find({}, {sort: {currentTimestamp: -1}, limit: 1}).toArray((err, result) => {
+            db.collection('timeline_summary').find({}, {sort: {currentTimestamp: -1}, limit: 2}).toArray((err, result) => {
                 if (err) res.send({"error": err});
-                res.send(result[0]);
+                const row = result[0];
+                const temp = {
+                    hari: row['hari_ke'],
+                    baru: row['kasus_baru'],
+                    kumulatif: row['kasus_kumulatif'],
+                    perawatan: row['dalam_perawatan'],
+                    perawatan_baru: row['dalam_perawatan'] - result[1]['dalam_perawatan'],
+                    sembuh: row['sembuh'],
+                    sembuh_baru: row['sembuh'] - result[1]['sembuh'],
+                    meninggal: row['meninggal'],
+                    meninggal_baru: row['meninggal'] - result[1]['meninggal'],
+                    update: row['currentTimestamp']
+                };
+                if (temp['baru'] === null) temp['baru'] = 0;
+                if (!temp['update']) temp['update'] = null;
+                res.send(temp);
             });
         }
     });
